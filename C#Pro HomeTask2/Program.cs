010cb38c935b73c7c15d9bc98bb0a277fe161e0c
Maksym Boiarynov <boyarinov.karazin@gmail.com>
2023-09-06 11:29:38 +0300

    Add project files.

---
+++ C#Pro HomeTask2/Program.cs
@@ -0,0 +1,27 @@
+﻿void cypher(string str, out string result, out string key)
+{
+    Random rnd = new Random();
+    int[] array = new int[str.Length];
+    result = "";
+    key = "";
+    for (int i = 0; i < str.Length; i++)
+    {
+        array[i] = rnd.Next(10);
+        key += array[i];
+        char s = str[i];
+        //Console.WriteLine(array[i]);
+        result += ((char)(s + array[i]));
+
+    }
+
+}
+
+    string result;
+    string key;
+    Console.WriteLine("Input some: ");
+    var str = Console.ReadLine();
+    cypher(str, out result, out key);
+    Console.WriteLine("This is key: ");
+    Console.WriteLine(key);
+    Console.WriteLine("This is result: ");
+    Console.WriteLine(result);
