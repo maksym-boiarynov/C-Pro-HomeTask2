848aacb14843b59ed5cacafdf67b3654900d10fd
Maksym Boiarynov <boyarinov.karazin@gmail.com>
2023-09-06 11:37:16 +0300

    Added decoder + if statement for choose decoder or encoder.

--- C#Pro HomeTask2/Program.cs
+++ C#Pro HomeTask2/Program.cs
@@ -16,6 +16,22 @@
 
 }
 
+void decoder(string str, string key, out string resultDecod)
+{
+    resultDecod = "";
+    for (int i = 0; i < str.Length; i++)
+    {
+        char s = str[i];
+        resultDecod += (char)(s - (key[i] - '0'));
+
+    }
+}
+
+Console.WriteLine("Enter number 1 or 2. Where 1 is for encoding and 2 is for decoding characters: ");
+int a = 1;
+a = Int32.Parse(Console.ReadLine());
+if (a <= 1)
+{
     string result;
     string key;
     Console.WriteLine("Input some: ");
@@ -25,3 +41,15 @@
     Console.WriteLine(key);
     Console.WriteLine("This is result: ");
     Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Input coded result: ");
+    var str = Console.ReadLine();
+    Console.WriteLine("Input key: ");
+    var key = Console.ReadLine();
+    Console.WriteLine("This is result: ");
+    string resultDecod;
+    decoder(str, key, out resultDecod);
+    Console.WriteLine(resultDecod);
+}
